211e93fe115d8f03c1e558dd3f4468d3c29bc28a
Sophia Greene <jxg1233@case.edu>
2026-06-18 15:40:36 -0400

    Latest commit message for the current commit: Add MIT license and setup configuration

--- src/__init__.py
+++ src/__init__.py
@@ -2,20 +2,45 @@ from chimerax.core.commands import CmdDesc, CmdDescArg, CmdDescKeyword, register
 from chimerax.core.toolshed import BundleAPI
 
 class _EpitopeMapperAPI(BundleAPI):
+    
+    # Required for newer API versions
+    api_version = 1 
+
     @staticmethod
-    def register_command(command_name, logger):
-        from .epitope_mapper import EpitopeMapper
+    def register_command(bi, ci, logger):
+        from .cmd import epitope_map
+        
+        # We define the command description here (moved from your old code)
         desc = CmdDesc(
-            required=[('model', CmdDescArg(str, help="The model ID"))],
+            required=[('atoms', CmdDescArg(str, help="The atoms to evaluate (e.g., :1)"))],
             keyword=[
-                ('output', CmdDescKeyword(str, help="Output file path")),
+                ('cutoff', CmdDescKeyword(float, help="Distance cutoff for spatial clustering")),
             ],
             synopsis='Map epitopes on protein structures.',
         )
-        register(command_name, desc, EpitopeMapper, logger=logger)
+        register(ci.name, desc, epitope_map, logger=logger)
 
     @staticmethod
-    def start_tool(session, tool_name, **kw):
+    def start_tool(session, bi, ti, **kw):
         pass
+
+    # --- NEW LIFECYCLE METHODS ---
+
+    @staticmethod
+    def initialize(session, bi):
+        # Called each time ChimeraX starts if custom initialization is set.
+        pass
+
+    @staticmethod
+    def finish(session, bi):
+        # Called when the bundle is deinitialized/unloaded. 
+        # Good for cleaning up global variables or temporary files.
+        pass
+
+    @staticmethod
+    def get_class(class_name):
+        # Called by session code to get a class from the bundle that was saved in a session.
+        # If you later create a UI tool (e.g., EpitopeMapperUI), return it here.
+        return None
 
 bundle_api = _EpitopeMapperAPI()
